8c9c49c7bc61a70e8d44a8a34efab1c3bfd96bb2
Vanessa <vsnakahara@gmail.com>
2017-02-03 00:32:52 -0200

    mudanca de Escala

--- js/transformacoes/mEscala.js
+++ js/transformacoes/mEscala.js
@@ -1,6 +1,6 @@
 
 function mEscala(sx, sy, mObj, tipo) {
-    console.log("entrei");
+    console.log("mobj");
     console.table(mObj);
 
     var dx, dy;
@@ -20,24 +20,26 @@ function mEscala(sx, sy, mObj, tipo) {
     }    
     
     var S = [
-        [sx, 0, (-dx)],
-        [0, sy, (-dy)],
+        [sx, 0, dx - (dx * sx)],
+        [0, sy, dy - (dy *   sy)],
         [0, 0, 1]
     ];
+    console.log("S");
     console.table(S);
 
-    var tRetorno = [
-        [1, 0, dx],
-        [0, 1, dy],
-        [0, 0, 1]
-    ];
-    console.table(tRetorno);
+    //var tRetorno = [
+    //    [1, 0, dx],
+    //    [0, 1, dy],
+     //   [0, 0, 1]
+    //];
+    //console.log("tRetorno");
+    //console.table(tRetorno);
 
-    var subResult = multiplyMatrices(S, tRetorno);
-    console.log("yeah");
-    console.table(subResult);
+    //var subResult = multiplyMatrices(tRetorno, S);
+    //console.log("subResult");
+    //console.table(subResult);
 
-    var result = multiplyMatrices(subResult, mObj);
+    var result = multiplyMatrices(S, mObj);
     console.log("Final");
     console.table(result);
    
@@ -48,6 +50,9 @@ function mEscala(sx, sy, mObj, tipo) {
         addDotToCanvas(result[0][0],result[1][0]);
         addDotToCanvas(result[0][2],result[1][2]);
 
+        console.log(result[0][0]);
+        console.log(result[1][0]);
+        
         var retangulo = new Rectangle(
                 {
                     p1: pontos[1],
