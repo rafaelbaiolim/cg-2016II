5998e44a7c307201b1a129a30ade4f0a85e9c217
Vanessa <vsnakahara@gmail.com>
2017-02-05 21:50:54 -0200

    Zoooooom Part |

--- js/transformacoes/zoomExtend.js
+++ js/transformacoes/zoomExtend.js
@@ -9,6 +9,10 @@
 // 	}	
 // };
 
+function calcularProporcao(largura, altura){
+	var R = this.largura / this.altura;
+}
+
 function zoomExtend(mousePos, tipo) {
 	var objetos = intersecaoPontos();
 	console.log("i"+objetos);
@@ -17,7 +21,34 @@ function zoomExtend(mousePos, tipo) {
 	// 		[0, 0, 1]];
 
 	var Tjv = [[Sx, 0 , -Sx*Xmin],
-		   [0, -Sy , -Sy*Ymin],
-		   [0, 0, 1]];		
+			   [0, -Sy , -Sy*Ymin],
+			   [0, 0, 1]
+			   ];
+
+	//calcular a proporcao Ratio da Janela e da Viewport
+	var rw, rv;
+	var translacaoVertical1 = [[x.Sx - Sx*Xmin],
+									 [-y*Sy - Sy*Ymin - ((vMax-vMaxNovo)/2)],
+									 [1]];
+
+	var translacaoVertical2 = [[x.Sx - Sx*Xmin - ((uMax-uMaxNovo)/2)],
+							   [-y*Sy - Sy*Ymin],
+							   [1]];
+	if(rw > rv){ 
+		//ajustar pela altura
+
+	}
+	if(rw < rv){
+		//ajustar pela largura
+	}	
+	//transladar para o centro
+	if(rw > rv){ 
+		//ajustar pela altura
+
+	}
+	if(rw < rv){
+		//ajustar pela largura
+	}	   		
 
 }
+
